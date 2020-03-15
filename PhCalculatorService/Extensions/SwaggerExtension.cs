020cc9f6498078f55402e1aeef38c3bb24db98ae
phfmleitao <phfm.leitao@gmail.com>
2020-03-15 21:11:17 +0000

    Criação do SwaggerExtension e ajuste do Startup

---
+++ PhCalculatorService/Extensions/SwaggerExtension.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+
+namespace PhCalculatorService.Extensions
+{
+    public static class SwaggerExtension
+    {
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(
+                    "v1",
+                    new OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = "PhCalculatorService API",
+                        Description = "API to do basic calculations, in order to be an easy-to-understand project for software development training in general.",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Pedro Henrique Fernandes Marques Leitão",
+                            Email = "phfm.leitao@gmail.com",
+                            Url = new Uri("https://github.com/phfmleitao")
+                        }
+                    }
+                );
+
+                //Adição do XMLDocumentation do projeto ao Swagger
+                string caminhoAplicacao = PlatformServices.Default.Application.ApplicationBasePath;
+                string nomeAplicacao = PlatformServices.Default.Application.ApplicationName;
+                string caminhoXmlDoc = Path.Combine(caminhoAplicacao, $"{nomeAplicacao}.xml");
+                c.IncludeXmlComments(caminhoXmlDoc);
+            });
+        }
+
+        public static void UseCustomSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PhCalculatorService API V1");
+            });
+        }
+    }
+}
--- PhCalculatorService/Startup.cs
+++ PhCalculatorService/Startup.cs
@@ -3,11 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.PlatformAbstractions;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.OpenApi.Models;
-using System.IO;
-using System;
+using PhCalculatorService.Extensions;
 
 namespace PhCalculatorService
 {
@@ -22,43 +18,25 @@ namespace PhCalculatorService
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().AddMvcOptions(options => options.EnableEndpointRouting = false);
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc(
-                    "v1", 
-                    new OpenApiInfo
-                    {
-                        Version = "v1",
-                        Title = "PhCalculatorService API",
-                        Description = "API to do basic calculations, in order to be an easy-to-understand project for software development training in general.",
-                        Contact = new OpenApiContact 
-                        { 
-                            Name = "Pedro Henrique Fernandes Marques Leitão",
-                            Email = "phfm.leitao@gmail.com",
-                            Url = new Uri("https://github.com/phfmleitao")
-                        }
-                    }
-                );
-
-                //Adição do XMLDocumentation do projeto ao Swagger
-                string caminhoAplicacao = PlatformServices.Default.Application.ApplicationBasePath;
-                string nomeAplicacao = PlatformServices.Default.Application.ApplicationName;
-                string caminhoXmlDoc = Path.Combine(caminhoAplicacao, $"{nomeAplicacao}.xml");
-                c.IncludeXmlComments(caminhoXmlDoc);
-            });
+            services.AddControllers();
+            services.AddSwagger();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PhCalculatorService API V1");
-            });
+            app.UseCustomSwagger();
 
-            app.UseMvc();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
